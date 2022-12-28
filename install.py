8dbf98b05a86f0203603c8766dbc2631b8f71c1a
Diego Feitosa <diegofeitosa@pop-os.localdomain>
2022-12-28 14:52:13 -0300

    FEAT-002: Adicionado um usuário padrão com acesso administrativo na instalação do projeto

--- install.py
+++ install.py
@@ -1,4 +1,7 @@
-from core.database import engine, DBBaseModel
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from core.database import engine, DBBaseModel, Session
+from core.security import crypt_hash
 
 async def create_tables() -> None:
     from account.models import AccountModel
@@ -10,7 +13,24 @@ async def create_tables() -> None:
 
     print('Tabelas criadas com sucesso...')
 
+async def create_admin() -> None:
+    from account.models import AccountModel
+    print('Criando o usuário Admin...')
+    async with engine.begin() as conn:
+        session: AsyncSession = Session()
+        account = AccountModel(
+            username="admin",
+            password=crypt_hash("admin"),
+            name="Administrador do Sistema",
+            activate=True,
+            admin=True
+        )
+        session.add(account)
+        await session.commit()
+    print('Usuário Admin criado com sucesso...')
+
 if __name__ == '__main__':
     import asyncio
     asyncio.run(create_tables())
+    asyncio.run(create_admin())
 
